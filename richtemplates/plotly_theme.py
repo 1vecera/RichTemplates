4357dd4d60e8675b02f6ac3ce62a0d70b7cf949d
Daniel Vecera <veceda@gmail.com>
2023-04-20 14:58:49 +0200

    added the template theme back

--- richtemplates/plotly_theme.py
+++ richtemplates/plotly_theme.py
@@ -1,4 +1,3 @@
-
 # %%
 from pathlib import Path
 import plotly
@@ -11,67 +10,94 @@ ytd_date_range = (datetime.datetime.now() - pd.offsets.YearBegin(), datetime.dat
 
 
 def generate_RichView_plotly_theme():
-    color_gridlines = 'rgba(238,238,238,0.9)'
-    color_ticks = 'rgba(0,0,0,0.9)'
+    template = go.layout.Template()
 
-    color_discrete_sequence = ["#9e2b25", "#223843", "#f9e784", "#a1b5d8", "#697a21", "#ff7f51",
-                               "#2191fb", "#dc7f9b", "#9cfffa", "#ece5f0"]
+    # Colors
+    template.layout.plot_bgcolor = "#FAF9F6"
+    template.layout.paper_bgcolor = "#FAF9F6"
+    template.layout.xaxis.gridcolor = "#CCC1B7"
+    template.layout.yaxis.gridcolor = "#CCC1B7"
+    template.layout.xaxis.tickcolor = "#4D4845"
+    template.layout.yaxis.tickcolor = "#4D4845"
+    template.layout.xaxis.title.font.color = "#4D4845"
+    template.layout.yaxis.title.font.color = "#4D4845"
 
+    # Grid
+    template.layout.xaxis.showgrid = False
+    template.layout.yaxis.showgrid = True
+    template.layout.xaxis.gridwidth = 0.2
+    template.layout.yaxis.gridwidth = 0.2
+    template.layout.xaxis.zeroline = False
+    template.layout.yaxis.zeroline = False
 
-    annotation_template = go.layout.Template()
-    annotation_template.data.scatter = [go.Scatter(hovertemplate=None, connectgaps=True)]
-    annotation_template.data.scattergl = [go.Scattergl(hovertemplate=None, connectgaps=True)]
+    # Ticks
+    template.layout.xaxis.tickmode = "auto"
+    template.layout.yaxis.tickmode = "auto"
+    template.layout.xaxis.ticklen = 3.5
+    template.layout.yaxis.ticklen = 0
+    template.layout.xaxis.tickwidth = 0.2
+    template.layout.yaxis.tickwidth = 0.2
 
-    annotation_template.layout.legend = go.layout.Legend(orientation='h',
-                                                         yanchor='top',
-                                                         xanchor='center',
-                                                         y=0.9,
-                                                         x=0.5,
-                                                         title=None)
+    # Lines
+    template.layout.colorway = [
+        "#186cac", "#81e7cc", "#eec8f1", "#dce475", "#86ec5a", "#eb70d5", "#829951", "#5a3386",
+        "#a50fa9", "#4ba40b"
+    ]
 
-    annotation_template.layout.font = go.layout.Font(family='Segoe UI',
-                                                     size=14)
-    annotation_template.layout.yaxis = go.layout.YAxis(showgrid=True,
-                                                       ##
-                                                       gridwidth=1,
-                                                       gridcolor=color_gridlines,
-                                                       ticks='outside',
-                                                       ##
-                                                       tickwidth=1,
-                                                       tickcolor=color_ticks,
-                                                       ticklen=5,
-                                                       ##
-                                                       zeroline=False,
-                                                       title=go.layout.yaxis.Title(text=None))
-    annotation_template.layout.xaxis = go.layout.XAxis(showgrid=True,
-                                                       gridwidth=1,
-                                                       gridcolor=color_gridlines,
-                                                       ##
-                                                       title=go.layout.xaxis.Title(text=None),
-                                                       ticks='outside',
-                                                       ##
-                                                       tickwidth=1,
-                                                       tickcolor=color_ticks,
-                                                       ticklen=10,
-                                                       ##
-                                                       showline=True,
-                                                       linewidth=1,
-                                                       linecolor='black',
-                                                       ##
-                                                       zeroline=False)
-    annotation_template.layout.hovermode = 'x'
-    annotation_template.layout.colorway = color_discrete_sequence
+    # Fonts
+    template.layout.font.family = "Lora, Palatino Linotype, Cambria, Century Gothic"
+    template.layout.font.size = 12
+    template.layout.xaxis.title.font.size = 10.8
+    template.layout.yaxis.title.font.size = 10.8
+    template.layout.xaxis.tickfont.size = 9.6
+    template.layout.yaxis.tickfont.size = 9.6
+    template.layout.legend.font.size = 9.6
 
-    config = dict({'scrollZoom': True})
+    # Legend
+    template.layout.legend.bgcolor = "rgba(0,0,0,0)"
+    template.layout.legend.xanchor = "left"
+    template.layout.legend.yanchor = "top"
+    template.layout.legend.x = 0
+    template.layout.legend.y = 1.05
 
-    return annotation_template
+    template.layout.legend.orientation = "h"
+    template.layout.legend.xanchor = "left"
+    template.layout.legend.valign = "top"
+    template.layout.legend.title.text = ""
+
+    # template.layout.legend.handletextpad = 0.8
+    # template.layout.legend.handlelength = 2
+    # template.layout.legend.labelspacing = 0.5
+
+    # Figure
+    template.layout.width = 600
+    template.layout.height = 400
+    template.layout.margin = dict(l=60, r=60, t=60, b=60)
+
+    # Axes
+
+    template.layout.xaxis.showspikes = False
+    template.layout.yaxis.showspikes = False
+    template.layout.xaxis.showline = True
+    template.layout.yaxis.showline = False
+    template.layout.xaxis.linewidth = 0.2
+    template.layout.yaxis.linewidth = 0.2
+    template.layout.xaxis.mirror = False
+    template.layout.yaxis.mirror = False
+    template.layout.xaxis.side = "bottom"
+    template.layout.yaxis.side = "left"
+
+    # Title and Suptitle
+    template.layout.title.x = 0
+    template.layout.title.font.size = 14.4
+    template.layout.title.xanchor = "left"
+    template.layout.title.xref = "paper"
+
+    return template
 
 
 def format_figure(fig: go.Figure):
-    fig.update_layout(xaxis_title=None,
-                      yaxis_title=None,
-                      legend_title=None,
-                      hovermode='x')
+    fig.update_layout(xaxis_title=None, yaxis_title=None, legend_title=None, hovermode='x')
     fig.update_xaxes(tickformat='%d %b %y')
     fig.update_traces(hovertemplate=None)
     return fig
@@ -86,55 +112,42 @@ def add_invasion_date_line(figure: go.Figure) -> go.Figure:
     return figure
 
 
-def add_date_line(color,
-                  comment,
-                  date,
-                  figure):
+def add_date_line(color, comment, date, figure):
     figure.add_vline(x=date, line=go.layout.shape.Line(width=1, color=color))
-    figure.add_annotation(x=date, xanchor='left', xshift=1, text=comment,
+    figure.add_annotation(x=date,
+                          xanchor='left',
+                          xshift=1,
+                          text=comment,
                           font=go.layout.annotation.Font(color=color),
                           showarrow=False)
     return figure
 
 
 def helper_range_selector():
-    return go.layout.xaxis.Rangeselector(
-
-        buttons=list([
-            go.layout.xaxis.rangeselector.Button(count=1,
-                                                 label="1m",
-                                                 step="month",
-                                                 stepmode="backward"),
-            go.layout.xaxis.rangeselector.Button(count=6,
-                                                 label="6m",
-                                                 step="month",
-                                                 stepmode="backward"),
-            go.layout.xaxis.rangeselector.Button(count=1,
-                                                 label="YTD",
-                                                 step="year",
-                                                 stepmode="todate"),
-            go.layout.xaxis.rangeselector.Button(count=1,
-                                                 label="1y",
-                                                 step="year",
-                                                 stepmode="backward"),
-            go.layout.xaxis.rangeselector.Button(step="all")
-        ]))
+    return go.layout.xaxis.Rangeselector(buttons=list([
+        go.layout.xaxis.rangeselector.Button(count=1, label="1m", step="month",
+                                             stepmode="backward"),
+        go.layout.xaxis.rangeselector.Button(count=6, label="6m", step="month",
+                                             stepmode="backward"),
+        go.layout.xaxis.rangeselector.Button(count=1, label="YTD", step="year", stepmode="todate"),
+        go.layout.xaxis.rangeselector.Button(count=1, label="1y", step="year", stepmode="backward"),
+        go.layout.xaxis.rangeselector.Button(step="all")
+    ]))
 
 
-def create_timerange_selector(fig: go.Figure,
-                              date_range=ytd_date_range) -> go.Figure:
+def create_timerange_selector(fig: go.Figure, date_range=ytd_date_range) -> go.Figure:
     # Add range slider
 
-    fig.update_layout(
-        xaxis=go.layout.XAxis(
-            range=ytd_date_range,
-            rangeselector=helper_range_selector(),
-            type="date"
-        )
-    )
+    fig.update_layout(xaxis=go.layout.XAxis(
+        range=ytd_date_range, rangeselector=helper_range_selector(), type="date"))
     return fig
 
-def export_chart(config:dict, figure:go.Figure, name:str, source_df:pd.DataFrame,  save_dir='out'):
+
+def export_chart(config: dict,
+                 figure: go.Figure,
+                 name: str,
+                 source_df: pd.DataFrame,
+                 save_dir='out'):
     save_dir = Path(save_dir)
     save_dir.mkdir(exist_ok=True)
 
